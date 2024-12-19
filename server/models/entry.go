6e2f1867d3e27fba107c5be3292cfb717b36f172
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2024-12-19 08:54:53 +0000

    created model for entry

--- server/models/entry.go
+++ server/models/entry.go
@@ -1,13 +1,14 @@
 package models
 
 import (
-    "go.mogodb.org/monogo-driver/bson/primitives"
+	"go.mogodb.org/monogo-driver/bson/primitives"
 )
 
+// go lang do not understand goland
 type Entry struct {
-	ID       primitives.ObjectID `bson:"_id,omitempty"`
-	Dish *string `bson:"dish"`
-	Fat *float64 `bson:"fat"`
-	Ingedients *string `bson:"ingedients"`
-	Calories *string `bson:"calories"`
+	ID         primitives.ObjectID `bson:"_id,omitempty"`
+	Dish       *string             `json:"dish"`
+	Fat        *float64            `json:"fat"`
+	Ingedients *string             `json:"ingedients"`
+	Calories   *string             `json:"calories"`
 }
