b22dfc5874f67e7d14a7d08e2ccda449b6a81b18
SAHIL YADAV <121011155+sahil-kumar-yadav@users.noreply.github.com>
2024-12-21 12:22:08 +0000

    outline of functions

--- server/routes/entries.go
+++ server/routes/entries.go
@@ -1,1 +1,31 @@
 package routes
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func AddEntry(c *gin.Context) {
+
+}
+
+func GetEntries(c *gin.Context) {
+
+}
+
+func GetEntriesByIngredients(c *gin.Context) {
+
+}
+
+func GetEntriesByID(c *gin.Context) {
+
+}
+
+func UpdateIngredient(c *gin.Context) {
+}
+func UpdateEntry(c *gin.Context) {
+	
+}
+
+func DeleteEntry(c *gin.Context) {
+
+}
